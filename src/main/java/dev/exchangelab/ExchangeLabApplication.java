94f64fd7110b4cccc5467e8f5fbe50bae510467b
Hello World <56444800+skatemonkey@users.noreply.github.com>
2026-06-12 20:05:30 +0800

    init

---
+++ src/main/java/dev/exchangelab/ExchangeLabApplication.java
@@ -0,0 +1,13 @@
+package dev.exchangelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeLabApplication.class, args);
+    }
+
+}
